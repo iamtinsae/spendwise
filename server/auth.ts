9cf4e789d16c0574ecfd145e5776d2141e8d900e
Tinsae <iamtinsae.dev@gmail.com>
2023-03-02 13:31:00 +0300

    feat: change signin path to '/'

--- server/auth.ts
+++ server/auth.ts
@@ -61,9 +61,9 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  // pages: {
-  //   signIn: '/',
-  // },
+  pages: {
+    signIn: '/',
+  },
 };
 
 /**
