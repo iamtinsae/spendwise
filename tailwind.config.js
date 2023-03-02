0dd8d303d6044de084c308c76e258a719b5287f0
Tinsae <iamtinsae.dev@gmail.com>
2023-03-02 13:35:50 +0300

    feat: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7FFFD4',
+        secondary: '#000000',
+      },
+    },
   },
   plugins: [],
-}
+};
