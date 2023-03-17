a9f6f522b480baccebd7a5359599eb2b9bd4c1d5
Tinsae <iamtinsae.dev@gmail.com>
2023-03-17 15:54:44 +0300

    chore(deps): install and configure @tremor/react@2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 
-!process.env.SKIP_ENV_VALIDATION && (await import("./env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./env.mjs'));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ['@tremor/react'],
   },
-}
+};
 
 export default nextConfig;
