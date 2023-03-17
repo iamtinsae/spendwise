4f8d6876a961cb88da9ab3352ad6f4151fd63991
Tinsae <iamtinsae.dev@gmail.com>
2023-03-17 16:50:27 +0300

    feat: add transaction creation functionality to the transactionsRouter

--- server/api/routes/transactions.ts
+++ server/api/routes/transactions.ts
@@ -1,7 +1,30 @@
+import { TransactionType } from '@prisma/client';
+import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
 
 export const transactionsRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
-    return ctx.prisma.transaction.findMany();
+    return ctx.prisma.transaction.findMany({
+      include: {
+        category: true,
+      },
+    });
   }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        amount: z.number(),
+        note: z.string(),
+        type: z.enum([TransactionType.DEPOSIT, TransactionType.WITHDRAW]),
+        categoryId: z.string().nullable(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.transaction.create({
+        data: {
+          ...input,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
 });
